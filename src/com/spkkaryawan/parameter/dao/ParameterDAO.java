b3ee94117440938b83f8fbd4777343d8c19921c5
AnggaAsmaraS <anggaasmara53@gmail.com>
2023-06-24 19:21:33 +0700

    membuat interface Parameterdao

--- src/com/spkkaryawan/parameter/dao/ParameterDAO.java
+++ src/com/spkkaryawan/parameter/dao/ParameterDAO.java
@@ -1,2 +1,17 @@
-package com.spkkaryawan.parameter.dao;public class ParameterDAO {
+package com.spkkaryawan.parameter.dao;
+
+import BaseDAO.BaseDAO;
+import com.spkkaryawan.parameter.model.Parameter;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface ParameterDAO extends BaseDAO {
+
+    int save (Parameter parameter) throws SQLException;
+    int update (Parameter parameter) throws SQLException;
+    int delete (String paramCode) throws SQLException;
+    List<Parameter> search() throws SQLException;
+    Parameter findById(String paramCode) throws SQLException;
+    int deleteChild(String paramCode) throws SQLException;
 }
